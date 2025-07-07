72b17a65242a105704412c9e798bc7615be201d3
Shrijana <shrijanabudhathoki51@gmail.com>
2025-07-07 07:37:23 +0545

    fix: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/blog-demo/', // replace 'blog-demo' with your repo name
+  base: command === 'serve' ? '/' : '/blog-demo/',  // <-- Add your repo name here
   server: {
     host: '0.0.0.0',
     port: 5173,
