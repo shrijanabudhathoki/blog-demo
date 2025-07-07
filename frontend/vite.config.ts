1e6a6d79cc9c0cc22b2a104628a518b9c2559d29
Shrijana <shrijanabudhathoki51@gmail.com>
2025-07-07 07:32:11 +0545

    fix: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-// https://vite.dev/config/
-export default {
+// vite.config.ts
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/blog-demo/',  // <-- Add this: your repo name with slashes
   server: {
-    host: '0.0.0.0', // Bind to all interfaces
+    host: '0.0.0.0',
     port: 5173,
   },
-}
+})
