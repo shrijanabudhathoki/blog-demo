1bda813a2ee2738bdf9ee9fed5c64c65a08011aa
Shrijana <shrijanabudhathoki51@gmail.com>
2025-07-09 18:04:38 +0545

    feat:test feature

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
+const base = process.env.VITE_BASE_PATH || '/';
 export default defineConfig(({ command }) => ({
-  const base = process.env.VITE_BASE_PATH || '/';
   server: {
     base,
     host: '0.0.0.0',
